b5e26485f7367d2f6a29315771cf72887dcd510c
snehjeetc12 <csnehjeet12@gmail.com>
2021-02-28 23:05:52 +0530

    Added method to check the player is winning or not

--- CallMain.java
+++ CallMain.java
@@ -4,12 +4,16 @@ import java.util.Scanner;
 public class CallMain{
     public static void main(String[] args){
         TicTacToeGame obj = new TicTacToeGame();
+        String player = "Player";
+        String computer = "Computer";
         obj.createBoard();
         Scanner sc =new Scanner(System.in);
         obj.setUserCharacter(sc);
         if( obj.toss(sc) ){
             System.out.println("You won, choose the position first!");
             obj.choosePosition(sc);
+            if(obj.checkWinner(player))
+                System.out.println("You won");
         }
         else{
             System.out.println("You lost, computer will choose the position first!");
--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -3,68 +3,139 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class TicTacToeGame{
-    private char[] board;
-    private char userChar;
+
+    private static final String player = "Player";
+    private static final String computer = "Computer";
+
+    private char[][] board;
+    private char playerChar;
     private char computerChar;
 
     TicTacToeGame(){
-        board = new char[10];
-        userChar = ' ';
+        board = new char[3][3];
+        playerChar = ' ';
         computerChar = ' ';
     }
 
     public void createBoard(){
-        for(int i=0; i<10; i++)
-            board[i] = ' ';
+        for(int i=0; i<3; i++)
+            for(int j=0; j<3; j++)
+                board[i][j] = ' ';
     }
 
     private void putBoardForPosition(){
         int count = 1;
-        System.out.println("------------");
-        for(int i=1; i<=3; i++){
-            for(int j=1; j<=3; j++){
+        System.out.println("-------------");
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
                 if(j == 1 || j == 3){
-                    if( board[count] == ' ')
+                    if( board[i][j] == ' ')
                         System.out.print(" " + count + " ");
                     else
-                        System.out.print(" " + board[count] + " ");
+                        System.out.print(" " + board[i][j] + " ");
                 }
                 else{
-                    if( board[count] == ' ' )
+                    if( board[i][j] == ' ' )
                         System.out.print("| " + count + " |");
                     else
-                        System.out.print("| " + board[count] + " |");
+                        System.out.print("| " + board[i][j] + " |");
                         
                 }
                 count++;
             }
             System.out.println();
-            System.out.println("------------");
+            System.out.println("-------------");
         }
     }
 
     public void setUserCharacter(Scanner sc){
         System.out.println("Enter the character (X or O): ");
-        userChar = sc.nextLine().toUpperCase().charAt(0);
-        computerChar = (userChar == 'X') ?  'O' : 'X' ; 
+        playerChar = sc.nextLine().toUpperCase().charAt(0);
+        computerChar = (playerChar == 'X') ?  'O' : 'X' ; 
     }
     
+    private Pair mapToIndices(int pos){
+        if(pos < 1 || pos > 9){
+            Pair p = new Pair(-1, -1);
+            return p;
+        }
+        Pair p = new Pair();
+        p.first = (pos - 1)/3;
+        p.second = (pos - 1)%3;
+        return p;
+    }
+
+    public boolean checkWinner(String user){
+        if(player.equals(user)){
+           return checkWinner(playerChar); 
+        }
+        else
+            return checkWinner(computerChar);
+    }
+
+    private boolean checkWinner(char filler){
+        //Horizontal check
+        for(int i=0; i<3; i++){
+            if(board[i][0] == board[i][1] && board[i][0] == board[i][2] 
+                    && board[i][0] == filler)
+                return true;
+        }
+
+        //Vertical check
+        for(int j=0; j<3; j++){
+            if(board[0][j] == board[1][j] && board[0][j] == board[2][j]
+                   && board[0][j] == filler)
+               return true; 
+        }
+
+        //Diagonal checks
+        //towards left
+        if(board [0][0] == filler){
+            boolean res = true;
+            for(int i=1; i<3; i++){
+                if( board[i][i] != filler){
+                    res = false;
+                    break;
+                }
+            }
+            if(res == true)
+                return true;
+        }
+        
+        //Diagonal check
+        //towards right
+        if(board [0][2] == filler){
+            boolean res = true;
+            for(int i=1,j=1; i<3 && j>=0 ; i++,j--){
+                if( board[i][j] != filler){
+                    res = false;
+                    break;
+                }
+            }
+            if(res == true)
+                return true;
+        }
+        return false;
+    }
+
     public void choosePosition(Scanner sc){
         System.out.println("Choose position (showing board): ");
         this.putBoardForPosition();
         System.out.println(": ");
         int pos = sc.nextInt();
         sc.nextLine();
-        while( (pos < 1 || pos > 9) || board[pos] != ' '){
-            if(pos < 1 || pos > 9)
+        Pair indices = mapToIndices(pos);
+        while( (indices.first == -1) || board[indices.first][indices.second] != ' '){
+            if(indices.first == -1)
                 System.out.println("Input out of bound"); 
             else
                 System.out.println("The position is already filled");
             System.out.println("Enter the position again: ");
             pos = sc.nextInt();
             sc.nextLine();
+            indices = mapToIndices(pos);
         }
-        board[pos] = userChar;
+        board[indices.first][indices.second] = playerChar;
     }
 
     public boolean toss(Scanner sc){
@@ -77,5 +148,18 @@ public class TicTacToeGame{
             return true;
         else
             return false;
+    }
+}
+
+
+class Pair{
+    public int first, second;
+    Pair(){
+        this.first = 0;
+        this.second = 0;
+    }
+    Pair(int x, int y){
+        this.first = x;
+        this.second = y;
     }
 }
